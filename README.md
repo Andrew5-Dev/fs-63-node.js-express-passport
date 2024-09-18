1789f72191c2814158a14e21cd80807d893719cc
Andrew5-Dev <topchiy.ap@gmail.com>
2024-09-18 11:02:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+#  API on Node.js і Express.js. use `celebrate` and `Joi` for validation.
+
+## Instructions
+
+npm install
+
+Run Development Server: 
+
+- npm run dev. 
+
+Run Production Server:
+
+
+- npm run start
+
+- The server is listening on port 3000
+
+Main Server routes:
+
+'/' 
+
+'/login' Passport authentication with Local Strategy by usersDB
+
+For test requests: POSTMAN https://www.postman.com
+
---
+++ src/routes/users.mjs
@@ -0,0 +1,29 @@
+import {Router} from 'express'
+import {
+    getUsersHandler,
+    postUsersHandler,
+    getUserByIdHandler,
+    putUserByIdHandler,
+    deleteUserByIdHandler,
+
+} from '../controllers/users.mjs'
+
+import {validateParamsUserId, validateUserPost, validateUserPut} from '../validators/userValidator.mjs'
+import validateUserInput from '../middlewares/validateUserInput.mjs'
+
+
+
+const usersRouter = Router()
+
+
+usersRouter.route('/')
+  .get(getUsersHandler)
+  .post(validateUserInput, validateUserPost,  postUsersHandler)
+
+usersRouter
+    .route('/:userId')
+    .get(validateParamsUserId, getUserByIdHandler)
+    .put(validateUserInput, validateParamsUserId, validateUserPut, putUserByIdHandler)
+    .delete(validateParamsUserId, deleteUserByIdHandler)
+
+export default usersRouter
